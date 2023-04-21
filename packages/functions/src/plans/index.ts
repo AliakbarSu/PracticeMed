9f8c30c72fcaebf77a2dd612d2bf270b9171724c
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-21 17:41:42 +1200

    Only return active plans

--- packages/functions/src/plans/index.ts
+++ packages/functions/src/plans/index.ts
@@ -3,18 +3,20 @@ import { getPlan, getPlans } from '@mpt-sst/core/plans/index'
 export const handler = ApiHandler(async (_evt) => {
   const plans = await (
     await getPlans()
-  ).map((plan) => ({
-    id: plan.id,
-    name: plan.name,
-    price: plan.metadata.price,
-    description: plan.description,
-    features: plan.description
-      ?.split('-')
-      .map((item) => item.trim())
-      .filter((item) => item.length > 0),
-    images: plan.images,
-    mostPopular: plan.metadata.mostPopular || false
-  }))
+  )
+    .filter((plan) => plan.active)
+    .map((plan) => ({
+      id: plan.id,
+      name: plan.name,
+      price: plan.metadata.price,
+      description: plan.description,
+      features: plan.description
+        ?.split('-')
+        .map((item) => item.trim())
+        .filter((item) => item.length > 0),
+      images: plan.images,
+      mostPopular: plan.metadata.mostPopular || false
+    }))
   return {
     body: JSON.stringify(plans)
   }
