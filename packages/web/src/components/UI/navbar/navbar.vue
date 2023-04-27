f0f839a6e5e0f48ecaf2cb8f0029221467224725
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-28 11:45:17 +1200

    Update login link

--- packages/web/src/components/UI/navbar/navbar.vue
+++ packages/web/src/components/UI/navbar/navbar.vue
@@ -43,7 +43,6 @@
               type="button"
               class="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 mr-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              <PlusIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
               Log In
             </button>
           </div>
