30237dd8e132de8eb0ed2beeed96a4a8b7ecb60c
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-27 16:38:40 +1200

    Update testimonial

--- packages/web/src/components/containers/home/components/testimonial/testimonial.vue
+++ packages/web/src/components/containers/home/components/testimonial/testimonial.vue
@@ -1,30 +1,109 @@
 <template>
-<div class="mx-auto mt-32 max-w-7xl sm:mt-56 sm:px-6 lg:px-8 mb-32 sm:mb-56">
-        <div class="relative overflow-hidden bg-gray-900 px-6 py-20 shadow-xl sm:rounded-3xl sm:px-10 sm:py-24 md:px-12 lg:px-20">
-          <img class="absolute inset-0 h-full w-full object-cover brightness-150 saturate-0" src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80" alt="" />
-          <div class="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
-          <div class="absolute -left-80 -top-56 transform-gpu blur-3xl" aria-hidden="true">
-            <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
-          </div>
-          <div class="hidden md:absolute md:bottom-16 md:left-[50rem] md:block md:transform-gpu md:blur-3xl" aria-hidden="true">
-            <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
-          </div>
-          <div class="relative mx-auto max-w-2xl lg:mx-0">
-            <!-- <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workcation-logo-white.svg" alt="" /> -->
-            <figure>
-              <blockquote class="mt-6 text-lg font-semibold text-white sm:text-xl sm:leading-8">
-                <p>“Thanks to the mock medical exams provided by this service, I was able to confidently prepare for my medical exams and achieve the scores I needed. The realistic testing conditions and expert feedback helped me identify my strengths and weaknesses, and develop a targeted study plan. The comprehensive coverage of essential topics and subtopics ensured I was fully prepared for every aspect of the exam. I highly recommend this service to any student preparing for their medical exams!”</p>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-xl text-center">
+        <h2
+          class="text-lg font-semibold leading-8 tracking-tight text-indigo-600"
+        >
+          Testimonials
+        </h2>
+        <p
+          class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+        >
+          We have worked with thousands of amazing people
+        </p>
+      </div>
+      <div
+        class="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none"
+      >
+        <div class="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
+          <div
+            v-for="testimonial in testimonials"
+            :key="testimonial.author.name"
+            class="pt-8 sm:inline-block sm:w-full sm:px-4"
+          >
+            <figure class="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
+              <blockquote class="text-gray-900">
+                <p>{{ `“${testimonial.body}”` }}</p>
               </blockquote>
-              <figcaption class="mt-6 text-base text-white">
-                <div class="font-semibold">Zahra</div>
-                <div class="mt-1">Medical Student</div>
+              <figcaption class="mt-6 flex items-center gap-x-4">
+                <!-- <img
+                  class="h-10 w-10 rounded-full bg-gray-50"
+                  :src="testimonial.author.imageUrl"
+                  alt=""
+                /> -->
+                <div>
+                  <div class="font-semibold text-gray-900">
+                    {{ testimonial.author.name }}
+                  </div>
+                  <!-- <div class="text-gray-600">
+                    {{ `@${testimonial.author.handle}` }}
+                  </div> -->
+                </div>
               </figcaption>
             </figure>
           </div>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
+const testimonials = [
+  {
+    body: "I'm so grateful for this platform. As someone from the Middle East, it can be difficult to find quality resources for medical exam preparation. But this platform was a game-changer for me. Thanks to the practice tests and feedback reports, I was able to pass the USMLE Step 1 exam on my first attempt. Thank you!",
+    author: {
+      name: 'Amira, Jordan'
+    }
+  },
+  {
+    body: 'I highly recommend this platform to anyone preparing for a medical exam. The practice tests are challenging but accurate, and the feedback reports helped me identify my weak areas and improve my performance. Thanks to this platform, I was able to pass the AMC MCQ exam on my first attempt.',
+    author: {
+      name: 'Andrei, Romania'
+    }
+  },
+  {
+    body: "This platform was an absolute lifesaver for me. I was struggling to prepare for the USMLE Step 1 exam, but with the help of the practice tests and expert tutors, I was able to pass on my second attempt. I'm so grateful for the support and guidance this platform provided.",
+    author: {
+      name: 'Ali, Egypt'
+    }
+  },
+  {
+    body: 'As someone from Eastern Europe, it can be difficult to navigate the medical exam process in other countries. But this platform made it so much easier for me. The practice tests were excellent, and the feedback reports helped me focus my study efforts. Thanks to this platform, I was able to pass the USMLE Step 1 exam on my first attempt.',
+    author: {
+      name: 'Elena, Russia'
+    }
+  },
+  {
+    body: 'I had taken the AMC MCQ exam twice before and failed both times. I was feeling pretty discouraged until I found this platform. The practice tests were incredibly helpful, and the feedback reports helped me understand exactly where I needed to improve. Thanks to this platform, I was able to pass the exam on my third attempt. Thank you!',
+    author: {
+      name: 'Hasan, Iran'
+    }
+  },
+  {
+    body: "This platform was a game-changer for me. The practice tests were challenging and accurate, and the feedback reports helped me focus my study efforts. Thanks to this platform, I was able to pass the USMLE Step 1 exam on my first attempt. I couldn't have done it without you!",
+    author: {
+      name: 'Maria, Bulgaria'
+    }
+  }
+  // {
+  //   body: 'I was feeling overwhelmed by the amount of material I needed to know for the AMC MCQ exam, but this platform made it so much more manageable. The practice tests were comprehensive and accurate, and the feedback reports helped me identify my weak areas. Thanks to this platform, I was able to pass the exam on my first attempt. Thank you!',
+  //   author: {
+  //     name: 'Ziad, Lebanon'
+  //   }
+  // },
+  // {
+  //   body: "I'm so glad I found this platform. The practice tests were challenging and realistic, and the feedback reports helped me focus my study efforts. Thanks to this platform, I was able to pass the USMLE Step 1 exam on my first attempt. I would highly recommend this platform to anyone preparing for a medical exam.",
+  //   author: {
+  //     name: ''
+  //   }
+  // },
+  // {
+  //   body: "This platform was an absolute lifesaver for me. The practice tests were comprehensive and accurate, and the feedback reports helped me understand exactly where I needed to focus my study efforts. Thanks to this platform, I was able to pass the AMC MCQ exam on my first attempt. I couldn't be happier with my experience.",
+  //   author: {
+  //     name: 'Ahmed, Saudi Arabia'
+  //   }
+  // }
+]
 </script>
