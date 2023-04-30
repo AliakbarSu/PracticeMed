706ff491b80c88dd236670ccb4c643b6afa2eb9e
Ali Sultani <aliakbar.su@hotmail.com>
2023-05-01 11:23:33 +1200

    Update get plan button

--- packages/web/src/components/containers/plans/Plans.vue
+++ packages/web/src/components/containers/plans/Plans.vue
@@ -30,8 +30,8 @@
             'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10'
           ]"
         >
-          <SkeletonLoading v-if="loading.plans" />
-          <div v-if="!loading.plans">
+          <SkeletonLoading v-if="loading" />
+          <div v-if="!loading">
             <div class="flex items-center justify-between gap-x-4">
               <h3
                 :id="tier.id"
@@ -77,59 +77,7 @@
               </li>
             </ul>
           </div>
-
-          <a
-            v-if="!loading.plans"
-            @click="
-              tier.freeTrial && !hasActivePlan
-                ? subscribeToFreeTrial(tier.id)
-                : subscribeToPlan(tier.id)
-            "
-            :aria-describedby="tier.id"
-            :class="[
-              tier.mostPopular
-                ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500'
-                : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300',
-              'mt-8 block rounded-md py-2 px-3 text-center cursor-pointer text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-            ]"
-          >
-            <div
-              role="status"
-              class="flex justify-center"
-              v-if="isCheckoutLoading(tier.id)"
-            >
-              <svg
-                aria-hidden="true"
-                class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              <span class="sr-only">Loading...</span>
-            </div>
-            <span
-              v-if="isSubscribedToPlan(tier.id) && !isCheckoutLoading(tier.id)"
-              >Active</span
-            >
-            <span
-              v-if="!isSubscribedToPlan(tier.id) && !isCheckoutLoading(tier.id)"
-            >
-              {{
-                tier.freeTrial && !hasActivePlan
-                  ? `Start ${tier.freeTrial} days free trial`
-                  : subscribeText(tier.id)
-              }}</span
-            >
-          </a>
+          <GetPlanButton :plan="tier" :loading="loading" />
         </div>
       </div>
     </div>
@@ -143,140 +91,37 @@ import { defineComponent } from 'vue'
 import axios from 'axios'
 import SkeletonLoading from './components/loading/CardLoading.vue'
 import type { Profile } from '@/types/user'
-import { addCheckoutEvent } from '@/gtag/index'
+
+import GetPlanButton from './components/Buttons/GetPlan.vue'
 
 export default defineComponent({
   components: {
     CheckIcon,
-    SkeletonLoading
+    SkeletonLoading,
+    GetPlanButton
   },
   data: () => {
     return {
-      loading: {
-        plans: false,
-        checkout: ''
-      },
+      loading: false,
       tiers: [{}, {}, {}] as Plan[],
       profile: {} as Profile
     }
   },
   async created() {
     this.getPlans()
-    this.getUserProfile()
   },
   methods: {
     async getPlans() {
-      this.loading.plans = true
+      this.loading = true
       try {
         const result = await axios.get(
           `${import.meta.env.VITE_API_ENDPOINT}/plans`
         )
         this.tiers = JSON.parse(result.data.body)
-      } catch (err) {}
-    },
-    async getUserProfile() {
-      try {
-        const token = await this.$auth0.getAccessTokenSilently()
-        const result = await axios.get(
-          `${import.meta.env.VITE_API_ENDPOINT}/user/profile`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          }
-        )
-        this.profile = JSON.parse(result.data.body)
-      } finally {
-        this.loading.plans = false
-      }
-    },
-    async subscribeToPlan(planId: string) {
-      this.loginIfNotAuthenticated(planId)
-      if (this.isSubscribedToPlan(planId)) return
-      this.loading.checkout = planId
-      try {
-        const checkoutUrl = await axios.get(
-          `${import.meta.env.VITE_API_ENDPOINT}/plans/${planId}/subscribe`
-        )
-        addCheckoutEvent('begin_checkout', {
-          ecommerce: {
-            currency: 'USD',
-            value: 9.99,
-            coupon: 'SUMMER_FUN',
-            items: [
-              {
-                item_id: 'SKU_12345',
-                item_name: 'Stan and Friends Tee',
-                affiliation: 'Google Merchandise Store',
-                coupon: 'SUMMER_FUN',
-                discount: 2.22,
-                index: 0,
-                item_brand: 'Google',
-                item_category: 'Apparel',
-                item_category2: 'Adult',
-                item_category3: 'Shirts',
-                item_category4: 'Crew',
-                item_category5: 'Short sleeve',
-                item_list_id: 'related_products',
-                item_list_name: 'Related Products',
-                item_variant: 'green',
-                location_id: 'ChIJIQBpAG2ahYAR_6128GcTUEo',
-                price: 9.99,
-                quantity: 1
-              }
-            ]
-          }
-        })
-        window.location.replace(checkoutUrl.data.body)
-      } finally {
-        this.loading.checkout = ''
-      }
-    },
-    async subscribeToFreeTrial(planId: string) {
-      this.loginIfNotAuthenticated(planId)
-      if (this.isSubscribedToPlan(planId)) return
-      this.loading.checkout = planId
-      try {
-        const checkoutUrl = await axios.get(
-          `${
-            import.meta.env.VITE_API_ENDPOINT
-          }/plans/${planId}/subscribe/free-trial`
-        )
-        window.location.replace(checkoutUrl.data.body)
-      } finally {
-        this.loading.checkout = ''
-      }
-    },
-    isCheckoutLoading(planId: string) {
-      return this.loading.checkout === planId
-    },
-    isSubscribedToPlan(planId: string) {
-      return this.profile?.plan?.id === planId
-    },
-    subscribeText(planId: string) {
-      const isSbuscribed = this.isSubscribedToPlan(planId)
-      const hasPlan = this.hasActivePlan
-      const currentPlanPrice =
-        this.tiers.find((tier) => tier.id === this.profile?.plan?.id)?.price ||
-        0
-      const planPrice =
-        this.tiers.find((tier) => tier.id === planId)?.price || 0
-      const changePlanText =
-        Number(currentPlanPrice) < Number(planPrice) ? 'Upgrade' : 'Downgrade'
-      return isSbuscribed ? 'Active' : hasPlan ? changePlanText : 'Get plan'
-    },
-    async loginIfNotAuthenticated(planId: string) {
-      try {
-        await this.$auth0.getAccessTokenSilently()
       } catch (err) {
-        this.$router.push('/account')
-        return
+      } finally {
+        this.loading = false
       }
-    }
-  },
-  computed: {
-    hasActivePlan() {
-      return !!this.profile?.plan?.id
     }
   }
 })
---
+++ packages/web/src/components/containers/plans/components/Buttons/GetPlan.vue
@@ -0,0 +1,147 @@
+<template>
+  <a
+    v-if="!loading"
+    @click="subscribe"
+    :aria-describedby="plan?.id"
+    :class="[
+      plan?.mostPopular
+        ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500'
+        : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300',
+      'mt-8 block rounded-md py-2 px-3 text-center cursor-pointer text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+    ]"
+  >
+    <div role="status" class="flex justify-center" v-if="subscribing">
+      <svg
+        aria-hidden="true"
+        class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
+
+    <span v-else-if="hasActivePlan && hasThisPlan">Active</span>
+    <span v-else-if="hasActivePlan">Switch plan</span>
+    <span v-else-if="plan.freeTrial">
+      {{ `Start ${plan.freeTrial} days free trial` }}
+    </span>
+    <span v-else> Get plan </span>
+  </a>
+</template>
+
+<script lang="ts" setup>
+import axios from 'axios'
+import { defineProps, onMounted } from 'vue'
+import { useAuth0 } from '@auth0/auth0-vue'
+import { ref, computed } from 'vue'
+import type { UserAppMetadata } from '@/types/user'
+import type { PropType } from 'vue'
+import type { Plan } from '@/types/plans'
+import { useRouter } from 'vue-router'
+import { addCheckoutEvent } from '@/gtag/index'
+
+const { getAccessTokenSilently, isAuthenticated } = useAuth0()
+const router = useRouter()
+
+const { loading, plan } = defineProps({
+  plan: {
+    type: Object as PropType<Plan>,
+    default: {}
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const getUserProfile = async () => {
+  try {
+    profileLoading.value = true
+    const token = await getAccessTokenSilently()
+    const result = await axios.get(
+      `${import.meta.env.VITE_API_ENDPOINT}/user/profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    profile.value = JSON.parse(result.data.body)
+  } finally {
+    profileLoading.value = false
+  }
+}
+
+onMounted(async () => {
+  await getUserProfile()
+})
+
+const profile = ref({} as UserAppMetadata)
+const profileLoading = ref(false)
+const subscribing = ref(false)
+
+const hasActivePlan = computed(() => !!profile.value.plan?.id)
+const hasThisPlan = computed(() => profile.value.plan?.id === plan?.id)
+
+const loginIfNotAuthenticated = () => {
+  if (!isAuthenticated) {
+    router.push('/account')
+    return
+  }
+}
+
+const subscribe = async () => {
+  loginIfNotAuthenticated()
+  if (hasThisPlan) return
+  subscribing.value = true
+  try {
+    const url = `${import.meta.env.VITE_API_ENDPOINT}/plans/${plan.id}`
+    const endpoint = plan.freeTrial
+      ? `${url}/subscribe/free-trial`
+      : `${url}/subscribe`
+    const checkoutUrl = await axios.get(endpoint)
+    addCheckoutEvent('begin_checkout', {
+      ecommerce: {
+        currency: 'USD',
+        value: 9.99,
+        coupon: 'SUMMER_FUN',
+        items: [
+          {
+            item_id: 'SKU_12345',
+            item_name: 'Stan and Friends Tee',
+            affiliation: 'Google Merchandise Store',
+            coupon: 'SUMMER_FUN',
+            discount: 2.22,
+            index: 0,
+            item_brand: 'Google',
+            item_category: 'Apparel',
+            item_category2: 'Adult',
+            item_category3: 'Shirts',
+            item_category4: 'Crew',
+            item_category5: 'Short sleeve',
+            item_list_id: 'related_products',
+            item_list_name: 'Related Products',
+            item_variant: 'green',
+            location_id: 'ChIJIQBpAG2ahYAR_6128GcTUEo',
+            price: 9.99,
+            quantity: 1
+          }
+        ]
+      }
+    })
+    window.location.replace(checkoutUrl.data.body)
+  } finally {
+    subscribing.value = false
+  }
+}
+</script>
