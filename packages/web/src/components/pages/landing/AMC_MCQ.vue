b5de7bb897dec107d72176abac06178ccc2f4fbd
Ali Sultani <aliakbar.su@hotmail.com>
2023-05-08 13:03:26 +1200

    Move testimonial section up

--- packages/web/src/components/pages/landing/AMC_MCQ.vue
+++ packages/web/src/components/pages/landing/AMC_MCQ.vue
@@ -149,63 +149,6 @@
         </div>
       </div>
 
-      <!-- Feature section -->
-      <div class="mt-32 sm:mt-56">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl sm:text-center">
-            <h2 class="text-base font-semibold leading-7 text-indigo-600">
-              Everything you need to prepare for your AMC MCQ exam
-            </h2>
-            <p
-              class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
-            >
-              Not ready to make a commitment yet? No problem.
-            </p>
-            <p class="mt-6 text-lg leading-8 text-gray-600">
-              Start with our free trial and decide later.
-            </p>
-          </div>
-        </div>
-        <div class="relative overflow-hidden pt-16">
-          <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <img
-              src="https://res.cloudinary.com/dxuf2ssx6/image/upload/v1682630656/practiceMed/Illustrations/hero_image.png"
-              alt="App screenshot"
-              class="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
-              width="2432"
-              height="1442"
-            />
-            <div class="relative" aria-hidden="true">
-              <div
-                class="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
-          <dl
-            class="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16"
-          >
-            <div
-              v-for="feature in features"
-              :key="feature.name"
-              class="relative pl-9"
-            >
-              <dt class="inline font-semibold text-gray-900">
-                <component
-                  :is="feature.icon"
-                  class="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                  aria-hidden="true"
-                />
-                {{ feature.name }}
-              </dt>
-              {{ ' ' }}
-              <dd class="inline">{{ feature.description }}</dd>
-            </div>
-          </dl>
-        </div>
-      </div>
-
       <!-- Testimonial section -->
       <div
         class="relative z-10 mt-32 bg-gray-900 pb-20 sm:mt-56 sm:pb-24 xl:pb-0"
@@ -286,6 +229,63 @@
               </figcaption>
             </figure>
           </div>
+        </div>
+      </div>
+
+      <!-- Feature section -->
+      <div class="mt-32 sm:mt-56">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl sm:text-center">
+            <h2 class="text-base font-semibold leading-7 text-indigo-600">
+              Everything you need to prepare for your AMC MCQ exam
+            </h2>
+            <p
+              class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            >
+              Not ready to make a commitment yet? No problem.
+            </p>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+              Start with our free trial and decide later.
+            </p>
+          </div>
+        </div>
+        <div class="relative overflow-hidden pt-16">
+          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <img
+              src="https://res.cloudinary.com/dxuf2ssx6/image/upload/v1682630656/practiceMed/Illustrations/hero_image.png"
+              alt="App screenshot"
+              class="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+              width="2432"
+              height="1442"
+            />
+            <div class="relative" aria-hidden="true">
+              <div
+                class="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl
+            class="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16"
+          >
+            <div
+              v-for="feature in features"
+              :key="feature.name"
+              class="relative pl-9"
+            >
+              <dt class="inline font-semibold text-gray-900">
+                <component
+                  :is="feature.icon"
+                  class="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                  aria-hidden="true"
+                />
+                {{ feature.name }}
+              </dt>
+              {{ ' ' }}
+              <dd class="inline">{{ feature.description }}</dd>
+            </div>
+          </dl>
         </div>
       </div>
 
