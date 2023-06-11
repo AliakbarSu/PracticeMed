1099af5e85de872c15f71cf73a585d2d3f0c7289
Ali Sultani <aliakbar.su@hotmail.com>
2023-06-12 11:21:20 +1200

    Update hero text

--- packages/nuxt/components/Home/components/UI/hero/hero.vue
+++ packages/nuxt/components/Home/components/UI/hero/hero.vue
@@ -34,8 +34,7 @@
           <h1
             class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
           >
-            Boost Your Scores with Our Mock Medical Exams - Revolutionize Your
-            Test Prep!
+            We can help you pass your Medical Licensing Exam!
           </h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">
             Experience Realistic Testing Conditions, Expert Feedback, and
