89cae22b7c2c68a34f54d1554c2a8bd2297ced0b
Ali Sultani <aliakbar.su@hotmail.com>
2023-08-14 17:54:20 +1200

    Fix link

--- packages/nuxt/components/Test/components/UI/CTA/UpgradeCTA.vue
+++ packages/nuxt/components/Test/components/UI/CTA/UpgradeCTA.vue
@@ -9,11 +9,10 @@
           {{ description }}
         </p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
+          <span
             @click="navigateToPlans"
-            href="#"
             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >Upgrade plan</a
+            >Upgrade plan</span
           >
           <a
             v-if="goBack"
