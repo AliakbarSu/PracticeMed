644c989a0ab536ba1e7cd138cd1379ec21310d6e
Ali Sultani <aliakbar.su@hotmail.com>
2023-09-22 07:00:47 +1200

    Add personalised tips function

---
+++ packages/core/src/personalised_tips/index.ts
@@ -0,0 +1,35 @@
+import { Config } from 'sst/node/config'
+import { ChatGPTAPI } from 'chatgpt'
+import { TestPerformanceResult } from '../../types/Result'
+import { getUserAppMetadata } from '../auth0'
+
+export const generate_tips = async ({
+  testId,
+  userId
+}: {
+  testId: string
+  userId: string
+}): Promise<string> => {
+  const userAppMetadata = await getUserAppMetadata(userId)
+  const test = userAppMetadata.test_history.find((test) => test.id === testId)
+
+  if (!test) {
+    return Promise.reject('Test not found')
+  }
+
+  // TODO: Uncomment this when we have a valid API key
+
+  // const api = new ChatGPTAPI({
+  //   apiKey: Config.SENDGRID_API_KEY
+  // })
+
+  const command = `
+    analyse this information and give me some meaningful feedback,
+    Give me in the form of bullet points, Don't mention seeking guidance, Generate maximum 5 tips,
+    Write it in html markup and only include ul tag
+    ${test.stats}
+  `
+  // const res = await api.sendMessage(command)
+  // return res.text
+  return ''
+}
