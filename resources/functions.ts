1507e32f208870f16778239e1497f152131085e4
Ali Sultani <aliakbar.su@hotmail.com>
2023-12-19 21:32:19 +1300

    move function paths to resources folder

---
+++ resources/functions.ts
@@ -0,0 +1,23 @@
+const fnPath = 'packages/functions/src'
+export const functions = {
+  profile: `${fnPath}/user/index.profile`,
+  reset_password: `${fnPath}/reset_password/index.handler`,
+  get_plans: `${fnPath}/plans/index.handler`,
+  get_plan: `${fnPath}/plans/index.getSinglePlan`,
+  get_checkout_url: `${fnPath}/plans/subscribe.checkoutUrl`,
+  get_checkout_url_free_trial: `${fnPath}/plans/subscribe.checkoutUrlWithFreeTrial`,
+  get_billing_link: `${fnPath}/user/index.billingLink`,
+  subscribe: `${fnPath}/plans/subscribe.handler`,
+  get_tests: `${fnPath}/list_tests/index.handler`,
+  get_single_test: `${fnPath}/get_test/index.handler`,
+  get_test_history: `${fnPath}/get_test/index.history`,
+  load_test: `${fnPath}/load_test/index.handler`,
+  get_test_result: `${fnPath}/result/index.handler`,
+  stripe_webhook: `${fnPath}/webhook/stripe.handler`,
+  newsletter_signup: `${fnPath}/newsletter/index.signup`,
+  newsletter_unsubscribe: `${fnPath}/newsletter/index.unsubscribe`,
+  get_tips: `${fnPath}/personalised_tips/index.handler`,
+  send_welcome_message: `${fnPath}/welcome_email/index.message`,
+  contact_form: `${fnPath}/contact/index.message`,
+  send_daily_recalls: `${fnPath}/daily_recalls/index.empty`
+}
--- stacks/PracticeMedApi.ts
+++ stacks/PracticeMedApi.ts
@@ -2,6 +2,7 @@ import { StackContext, Api, Cron } from 'sst/constructs'
 import { endpoints } from '../resources/endpoints'
 import { configure_parameters } from './Parameters'
 import { dev } from '../resources/stages'
+import { functions } from '../resources/functions'
 
 export function API(context: StackContext) {
   const { stack } = context
@@ -18,7 +19,6 @@ export function API(context: StackContext) {
     SENDGRID_API_KEY
   } = configure_parameters(context)
 
-  const fnPath = 'packages/functions/src'
   const api = new Api(stack, 'api', {
     customDomain: {
       domainName:
@@ -49,60 +49,60 @@ export function API(context: StackContext) {
       // User
       'GET /api/user/profile': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/user/index.profile`
+        function: functions.profile
       },
       'GET /api/user/reset-password': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/reset_password/index.handler`
+        function: functions.reset_password
       },
       // PLANS
-      'GET /api/plans': `${fnPath}/plans/index.handler`,
-      'GET /api/plans/{id}': `${fnPath}/plans/index.getSinglePlan`,
+      'GET /api/plans': functions.get_plans,
+      'GET /api/plans/{id}': functions.get_plan,
       'GET /api/plans/{id}/subscribe': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/plans/subscribe.checkoutUrl`
+        function: functions.get_checkout_url
       },
       'GET /api/plans/{id}/subscribe/free-trial': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/plans/subscribe.checkoutUrlWithFreeTrial`
+        function: functions.get_checkout_url_free_trial
       },
       'GET /api/billing/manage': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/user/index.billingLink`
+        function: functions.get_billing_link
       },
       'POST /api/plans/{id}/subscribe': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/plans/subscribe.handler`
+        function: functions.subscribe
       },
       // TESTS
       'GET /api/tests': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/list_tests/index.handler`
+        function: functions.get_tests
       },
       'GET /api/tests/history': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/get_test/index.history`
+        function: functions.get_test_history
       },
-      'GET /api/tests/{id}': `${fnPath}/get_test/index.handler`,
+      'GET /api/tests/{id}': functions.get_single_test,
       'GET /api/tests/{id}/load': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/load_test/index.handler`
+        function: functions.load_test
       },
       'POST /api/test/{id}/result': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/result/index.handler`
+        function: functions.get_test_result
       },
       // WEBHOOKS
-      'POST /api/webhooks/stripe': `${fnPath}/webhook/stripe.handler`,
+      'POST /api/webhooks/stripe': functions.stripe_webhook,
       // New Letter
-      'POST /api/newsletter/signup': `${fnPath}/newsletter/index.signup`,
-      'POST /api/newsletter/unsubscribe': `${fnPath}/newsletter/index.unsubscribe`,
+      'POST /api/newsletter/signup': functions.newsletter_signup,
+      'POST /api/newsletter/unsubscribe': functions.newsletter_unsubscribe,
       // CONTACTS
-      'POST /api/contact/message': `${fnPath}/contact/index.message`,
-      'POST /api/auth/welcome': `${fnPath}/welcome_email/index.message`,
+      'POST /api/contact/message': functions.contact_form,
+      'POST /api/auth/welcome': functions.send_welcome_message,
       'GET /api/tests/{id}/personalised-tips': {
         authorizer: 'auth0Authorizer',
-        function: `${fnPath}/personalised_tips/index.handler`
+        function: functions.get_tips
       }
       // AUTOMATIONS
       // 'POST /api/daily-recalls': `${fnPath}/daily_recalls/index.send`
@@ -120,8 +120,8 @@ export function API(context: StackContext) {
         },
         handler:
           stack.stage === dev
-            ? `${fnPath}/daily_recalls/index.empty`
-            : `${fnPath}/daily_recalls/index.empty`
+            ? functions.send_daily_recalls
+            : functions.send_daily_recalls
       }
     }
   })
