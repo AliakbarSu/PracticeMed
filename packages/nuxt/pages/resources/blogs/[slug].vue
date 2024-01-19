839ad4acb25d4a7129b0cdcc3561759a28179eaa
Ali Sultani <aliakbar.su@hotmail.com>
2024-01-19 23:24:50 +1300

    Add blogs section

---
+++ packages/nuxt/pages/resources/blogs/[slug].vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white px-6 py-32 lg:px-8">
+    <div
+      v-if="!pending"
+      class="mx-auto max-w-3xl text-base leading-7 text-gray-700"
+    >
+      <p class="text-base font-semibold leading-7 text-indigo-600">
+        {{ data.subtitle }}
+      </p>
+      <h1
+        class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+      >
+        {{ data.title }}
+      </h1>
+      <div v-html="data.body.html"></div>
+      <figure class="hidden mt-16">
+        <img
+          class="aspect-video rounded-xl bg-gray-50 object-cover"
+          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
+          alt=""
+        />
+        <figcaption class="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
+          <InformationCircleIcon
+            class="mt-0.5 h-5 w-5 flex-none text-gray-300"
+            aria-hidden="true"
+          />
+          Faucibus commodo massa rhoncus, volutpat.
+        </figcaption>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const { data, pending, error, refresh } = await useFetch(
+  `/api/blogs/${route.params.slug}`
+)
+</script>
---
+++ packages/nuxt/pages/resources/blogs/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Resources
+        </h2>
+        <p class="mt-2 text-lg leading-8 text-gray-600">
+          Learn how to pass your medical licensing exam with our great
+          resources.
+        </p>
+        <div
+          v-if="!pending"
+          class="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16"
+        >
+          <article
+            v-for="post in data"
+            :key="post.id"
+            class="flex max-w-xl flex-col items-start justify-between"
+          >
+            <div class="flex items-center gap-x-4 text-xs">
+              <time :datetime="post.publishedOn" class="text-gray-500">{{
+                new Date(post.publishedOn).toLocaleDateString('en-CA', {
+                  month: 'long',
+                  year: 'numeric',
+                  day: 'numeric'
+                })
+              }}</time>
+              <NuxtLink
+                as="as"
+                :href="`/resources/blogs/${post.slug}`"
+                class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >{{ post.topic }}</NuxtLink
+              >
+            </div>
+            <div class="group relative">
+              <h3
+                class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+              >
+                <a :href="`/resources/blogs/${post.slug}`">
+                  <span class="absolute inset-0" />
+                  {{ post.title }}
+                </a>
+              </h3>
+              <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                {{ post.body.text }}
+              </p>
+            </div>
+            <div class="hidden relative mt-8 items-center gap-x-4">
+              <!-- <img
+                :src="post.author.imageUrl"
+                alt=""
+                class="h-10 w-10 rounded-full bg-gray-50"
+              /> -->
+              <!-- <div class="text-sm leading-6">
+                <p class="font-semibold text-gray-900">
+                  <a :href="post.author.href">
+                    <span class="absolute inset-0" />
+                    {{ post.author.name }}
+                  </a>
+                </p>
+                <p class="text-gray-600">{{ post.author.role }}</p>
+              </div> -->
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data, pending, error, refresh } = await useFetch(`/api/blogs`)
+
+useSeoMeta({
+  title:
+    'Resource about how to prepare and pass your AMC MCQ exam | Practice Med',
+  description:
+    'Resrouces to prepare for your medical licensing exam including AMC MCQ'
+})
+</script>
