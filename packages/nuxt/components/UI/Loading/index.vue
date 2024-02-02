1b2221db359144f9513e2aee8beaae8883ab7f81
Ali Sultani <aliakbar.su@hotmail.com>
2024-02-02 16:09:49 +1300

    Add full page spinner

--- packages/nuxt/components/UI/Loading/index.vue
+++ packages/nuxt/components/UI/Loading/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div v-if="loading" class="fixed left-0 top-0 h-2 w-full z-50 bg-green-500" />
+  <Teleport to="body">
+    <div
+      v-if="loading"
+      class="bg-gray-200/85 fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center z-40"
+    >
+      <div
+        class="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin absolute"
+      ></div>
+    </div>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
