3184cfcc4e7a2858f43b009724ad15c478a52713
Ali Sultani <aliakbar.su@hotmail.com>
2024-02-03 11:41:14 +1300

    Add result redirect component

---
+++ packages/nuxt/components/Home/components/test/viewResultsRedirect.vue
@@ -0,0 +1,15 @@
+<template></template>
+<script lang="ts" setup>
+const props = defineProps<{
+  testId: string | null
+}>()
+
+watch(
+  () => props.testId,
+  async (testId) => {
+    if (testId) {
+      await navigateTo(`/results/${testId}`)
+    }
+  }
+)
+</script>
