ed175f2b808f150ed8de0db7f9af5593ee2713a0
Ali Sultani <aliakbar.su@hotmail.com>
2024-02-15 11:00:02 +1300

    Use cloudinary for og image

--- packages/nuxt/pages/questions/[id].vue
+++ packages/nuxt/pages/questions/[id].vue
@@ -6,18 +6,20 @@
 
 <script lang="ts" setup>
 import { type Question } from "../../src/types/question";
-import { fetchOgImage, fetchPublicQuestionApi } from "../../src/api/adminApi";
+import { fetchPublicQuestionApi } from "../../src/api/adminApi";
 
 useSeoMeta({
   title: "PracticeMed Question",
   description: "AMC daily mock questions",
 });
 
+const route = useRoute();
+
 const question: Question = await fetchPublicQuestionApi();
-const image = await fetchOgImage();
+const id = route.params.id;
 
 useSeoMeta({
   ogTitle: "PracticeMed Question",
-  ogImage: image.url,
+  ogImage: `https://res.cloudinary.com/dxuf2ssx6/image/upload/v1707947397/practiceMed/questions/${id}.jpg`,
 });
 </script>
